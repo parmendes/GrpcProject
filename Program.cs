77b675a9ee8dd1e81b48db5e7abfa7fef9d948aa
parmendes <pedro.a.mendes@criticalsoftware.com>
2025-04-07 11:45:22 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,12 +1,20 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add gRPC services to the container
-builder.Services.AddGrpc();
+builder.Services
+  .AddCodeFirstGrpc()  // Registers gRPC services
+  .AddCodeFirstGrpcOpenApi(); // Enables OpenAPI support
+
+builder.Services.AddGrpcSwagger(); // Register Swagger
 
 var app = builder.Build();
 
 // Map the gRPC service
 app.MapGrpcService<GrpcProject.Services.GreetingService>();
+
+// Displays Swagger UI for documentation
+app.UseSwagger();
+app.UseSwaggerUI();
 
 // Fallback HTTP endpoint
 app.MapGet("/", () => "This is a gRPC server. Use a gRPC client to communicate.");
