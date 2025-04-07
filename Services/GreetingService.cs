5739b060f7421a1fb3e6e6da2dbd229cdd34534d
parmendes <pedro.a.mendes@criticalsoftware.com>
2025-04-07 10:16:50 +0100

    Create GreetingService.cs

---
+++ Services/GreetingService.cs
@@ -0,0 +1,18 @@
+using Grpc.Core;
+using MyGrpcProject;
+
+namespace MyGrpcProject.Services
+{
+    public class GreetingService : Greeter.GreeterBase
+    {
+        public override Task<GreetingReply> SayHello(GreetingRequest request, ServerCallContext context)
+        {
+            var reply = new GreetingReply
+            {
+                Message = $"Hello, {request.Name}! Welcome to gRPC in C#."
+            };
+
+            return Task.FromResult(reply);
+        }
+    }
+}
