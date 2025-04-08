4f202c1115414cb497773cd636a3cd9a659ce073
parmendes <pedro.a.mendes@criticalsoftware.com>
2025-04-08 16:08:25 +0100

    move documentation files

--- Program.cs
+++ Program.cs
@@ -18,30 +18,3 @@ app.MapGrpcService<GrpcProject.Services.GreetingService>();
 app.MapGet("/", () => "This is a gRPC server. Use a gRPC client to communicate.");
 
 app.Run();
-
-
-// using Microsoft.AspNetCore.Builder;
-// using ProtoBuf.Grpc.Server;
-
-// var builder = WebApplication.CreateBuilder(args);
-
-// // Add gRPC services to the container
-// builder.Services
-//   .AddCodeFirstGrpc()  // Registers gRPC services
-//   .AddCodeFirstGrpcOpenApi(); // Enables OpenAPI support
-
-// builder.Services.AddGrpcSwagger(); // Register Swagger
-
-// var app = builder.Build();
-
-// // Map the gRPC service
-// app.MapGrpcService<GrpcProject.Services.GreetingService>();
-
-// // Displays Swagger UI for documentation
-// app.UseSwagger();
-// app.UseSwaggerUI();
-
-// // Fallback HTTP endpoint
-// app.MapGet("/", () => "This is a gRPC server. Use a gRPC client to communicate.");
-
-// app.Run();
